e47b6a473b6d3cfe664b97f23f27ea06fd470e54
herve <herve.ekemkingni@ictcg.com>
2020-02-06 14:17:03 +0100

    Add Class Address

---
+++ src/com/company/Address.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Address {
+
+    private String street;
+    private String number;
+    private String postalCode;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
